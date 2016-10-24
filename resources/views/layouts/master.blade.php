8720b29da10c84beb666dbf24e6ec754daaa13a1
Joe Phearse <jphearse@gmail.com>
2016-10-24 15:25:04 -0500

    created index page for events

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> @yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="icon" href="/jp.ico">
+    <style>
+    </style>
+</head>
+<body>
+	@include('layouts.partials.navbar')
+	
+	<div class="container">
+    	@yield('content')
+    </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
