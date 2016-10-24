c3af26ebff75a4d8181c6a617426ce4bdfb8828c
Liz Peacock <liz.peacock11@gmail.com>
2016-10-24 18:54:41 -0500

    created events migration

--- database/migrations/2016_10_24_232437_create_events_table.php
+++ database/migrations/2016_10_24_232437_create_events_table.php
@@ -14,6 +14,17 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->integer('band_id')->unsigned();
+            $table->foreign('band_id')->references('id')->on('bands');
+
+            $table->integer('venue_id')->unsigned();
+            $table->foreign('venue_id')->references('id')->on('venues');
+
+            $table->dateTime('event_time');
+            $table->decimal('price', 4, 2);
+            $table->text('buy_tickets');
+            
             $table->timestamps();
         });
     }
