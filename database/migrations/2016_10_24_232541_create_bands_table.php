6ad59fefeb75e65dead0eec97ee01908f8ed3b0b
Liz Peacock <liz.peacock11@gmail.com>
2016-10-24 18:31:59 -0500

    large commit created Band, Event, Genre, Venue models created db migrations - events, venues, genres, bands, users, rsvps, user_genres, user_bands

---
+++ database/migrations/2016_10_24_232541_create_bands_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bands', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bands');
+    }
+}
