54720774941a99a4680399fb008d9d00fef2915a
Joe Phearse <jphearse@gmail.com>
2016-10-25 14:59:05 -0500

    cleaned code

--- resources/views/events/all.blade.php
+++ resources/views/events/all.blade.php
@@ -7,14 +7,14 @@
 
 <div class="container">
 <h2>All Events</h2>
-@for($i = 1; $i < 4; $i++)
+@for($i = 1; $i < 10; $i++)
 	<div class="well">
       <div class="media">
       	<a class="pull-left" href="#">
     		<img class="media-object" src="http://placekitten.com/150/150">
   		</a>
   		<div class="media-body">
-    		<a href="/events/{{$i}}"><h4 class="media-heading">Event {{$i}}</h4></a>
+    		<a href="events/{{$i}}"><h4 class="media-heading">Event {{$i}}</h4></a>
           <p>Info</p>
           
        </div>
---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<div id="footer" class="navbar navbar-inverse">
+    <div class="container">
+      <p class="navbar-text pull-left">© 2016 - Musics.dev</p>
+      
+     {{--  <a href="http://youtu.be/zJahlKPCL9g" class="navbar-btn btn-danger btn pull-right">
+      <span class="fa fa-facebook"></span>  Subscribe on YouTube</a> --}}
+    </div>
