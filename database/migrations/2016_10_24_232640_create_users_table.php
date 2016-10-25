ae172e55a96641e3b7a33cb0b8f770f0487e758b
Liz Peacock <liz.peacock11@gmail.com>
2016-10-25 09:38:33 -0500

    accidently spun up 2 users migrations so I deleted one

--- database/migrations/2016_10_24_232640_create_users_table.php
+++ database/migrations/2016_10_24_232640_create_users_table.php
@@ -12,7 +12,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('users', function (Blueprint $table) {
+            
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->integer('zip_code');
+            $table->string('image_url');
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
