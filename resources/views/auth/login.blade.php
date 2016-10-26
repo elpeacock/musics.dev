f4a9bfc40b7f5732bc8d36eb398a2dbc622a76ad
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-10-26 11:45:19 -0500

    started working on dropdown for genres

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,3 +22,19 @@
 	</div>
 </div>
 @stop
+<html>
+  <head>
+    <link href="path/to/multiselect.css" media="screen" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <select multiple="multiple" id="my-select" name="my-select[]">
+      <option value='elem_1'>elem 1</option>
+      <option value='elem_2'>elem 2</option>
+      <option value='elem_3'>elem 3</option>
+      <option value='elem_4'>elem 4</option>
+      ...
+      <option value='elem_100'>elem 100</option>
+    </select>
+    <script src="path/to/jquery.multi-select.js" type="text/javascript"></script>
+  </body>
+</html>
