103530dd5c5f4b9e9ce4128cd3338333368d312a
Liz Peacock <liz.peacock11@gmail.com>
2016-10-25 22:00:35 -0500

    double sticky nav bar that (will) do stuff and things
    Css that makes nav bar kinda stand out (we can discuss color schemes tomorrow!)

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,41 +1,89 @@
+<!--navbars and things that do stuff-->
 
-<!-- Begin Navbar -->
-<div id="nav">
-  <div class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="100">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-               <span class="sr-only">Toggle navigation</span>
-               <span class="icon-bar"></span>
-               <span class="icon-bar"></span>
-               <span class="icon-bar"></span>
-           </button>
-          <a class="navbar-brand" href="#"><img class="img-responsive logo" src="https://cdn4.iconfinder.com/data/icons/adore/118/Camera.png" alt=""></a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
+<div class='fixed-top'>
 
+    <!--search nav-->
+    <nav class='nav-top'>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="{{action('EventsController@index')}}">Events</a></li>
+    <div class='container'>
 
-          <ul class="nav navbar-nav">
-                <li><a href="#">Events</a></li>
+    <div class='row'>
 
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Register</a></li>
-            </ul>
-            <form class="navbar-form navbar-right" method="GET">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search" name="search">
+        <form class="form-inline pull-right" role="search" method="GET" action="">
+                
+            <div class="input-group">
+
+                <input type="text" class="form-control" placeholder="Search" name="search" value="">
+                
+                <div class="input-group-btn">
+                    
+                    <button class="btn btn-default navbar-search" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                
                 </div>
-                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-            </form>
-        </div>
-        <!--/.nav-collapse -->
-      </div>
-      <!--/.contatiner -->
-</div>
+            
+            </div>
+
+        </form>
+                
+    </div>
+
+    </div>
+
+    </nav>
+    <!--/.nav-top search nav-->
+
+    <!--nav for errything else-->
+    <nav class="navbar navbar-default" role="navigation">
+
+    <div class="navbar-header">
+
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+
+            <span class="sr-only">Toggle navigation</span>
+            
+            <span class="icon-bar"></span>
+            
+            <span class="icon-bar"></span>
+            
+            <span class="icon-bar"></span>
+        
+        </button>
+
+        <a class="navbar-brand" href="">ShowUp<span class="flashy">.buzz</span></a>
+
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        
+        <ul class="nav navbar-nav">
+            
+            <li class="dropdown">
+                
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account<b class="caret"></b></a>
+                
+                <ul class="dropdown-menu">
+                    
+                    <li><a href="#">Account Info</a></li>
+                    
+                    <li><a href="#">Create An Event</a></li>
+                    
+                    <li class="divider"></li>
+                    
+                    <li><a href="#">Log Out</a></li>                 
+                </ul>
+            
+            <li><a href="#">Sign Up</a></li>
+            
+            <li><a href="#">Log In</a></li>
+
+            <li><a href="#">Events</a></li>
+            
+            </li>
+       
+        </ul>
+
+    </div>
+
+    </nav>
+
 </div>
