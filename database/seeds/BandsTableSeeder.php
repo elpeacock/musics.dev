a49576e9d8678e125554f5b2d8450c2ca017c2a7
Liz Peacock <liz.peacock11@gmail.com>
2016-10-26 14:30:48 -0500

    hardcoded some bands

--- database/seeds/BandsTableSeeder.php
+++ database/seeds/BandsTableSeeder.php
@@ -11,6 +11,20 @@ class BandsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('bands')->insert([
+        	'name' => 'red hot chili peppers',
+        	'genre_id' => '1',
+        	'owner_id' => '1',
+        	'description' => 'the raddest',
+        	'image_url' => 'animage.jpg',
+    	]);
+
+    	DB::table('bands')->insert([
+    		'name' => 'band',
+        	'genre_id' => '2',
+        	'owner_id' => '2',
+        	'description' => 'cool band',
+        	'image_url' => 'animage.jpg',
+		]);
     }
 }
