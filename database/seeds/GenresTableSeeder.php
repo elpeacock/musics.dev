a7f22347f9fcf42870bef94d770dd3f5c5081482
Liz Peacock <liz.peacock11@gmail.com>
2016-10-27 11:05:07 -0500

    altered the id on the genres table to just be an integer rather than auto incrementing 	-this means that users will NOT be able to add any genres but rather will have to choose from a preset list of genres. 	-i feel like this is totally ok for what we need genres to do.
    prepared the genres seeder to hold only music genres

--- database/seeds/GenresTableSeeder.php
+++ database/seeds/GenresTableSeeder.php
@@ -11,6 +11,59 @@ class GenresTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::unprepared(file_get_contents('./database/sql/genres.sql'));
+        DB::table('genres')->insert([
+
+        	'id' => 3,
+        	'name' => 'R&B/Urban Soul',
+    	]);
+
+        DB::table('genres')->insert([
+
+        	'id' => 5,
+        	'name' => 'Rock & Pop',
+    	]);
+
+    	DB::table('genres')->insert([
+
+    		'id' => 6,
+    		'name' => 'Alternative Rock',
+		]);
+
+		DB::table('genres')->insert([
+
+			'id' => 8, 
+			'name' => 'Rap & Hip Hop',
+		]);
+
+		DB::table('genres')->insert([
+
+			'id' => 9,
+			'name' => 'Country & Folk',
+		]);
+
+		DB::table('genres')->insert([
+
+			'id' => 12,
+			'name' => 'Jazz & Blues',
+		]);
+
+		DB::table('genres')->insert([
+
+			'id' => 16, 
+			'name' => 'Hard Rock/Metal',
+		]);
+
+		DB::table('genres')->insert([
+
+			'id' => 18,
+			'name' => 'Dance/Electronic',
+		]);
+
+		DB::table('genres')->insert([
+
+			'id' => 73,
+			'name' => 'Alternative & Indie',
+		]);
+
     }
 }
