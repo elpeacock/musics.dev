81f375bcc97680cc44811fa48dfa9ddfb8f5964c
Liz Peacock <liz.peacock11@gmail.com>
2016-10-28 16:39:22 -0500

    added cdns for bootstrap select plug-in

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,8 +2,13 @@
 <html lang="en">
 <head>
     <title> @yield('title')</title>
+    {{-- fontawesome --}}
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+    {{-- bootstrap --}}
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- bootstrap select plugin -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css">
+    {{-- custom css --}}
     <link rel="stylesheet" type="text/css" href="/css/mainCss.css">
 </head>
 <body>
@@ -14,8 +19,15 @@
 
     @include('layouts.partials.footer')
 
+    {{-- bootstrap jquery --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    {{-- bootstrap js --}}
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- bootstrap select plug-in JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/bootstrap-select.min.js"></script>
+    <!-- (Optional)JavaScript translation files for bootstrap select plugin-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/i18n/defaults-*.min.js"></script>
+
 {{--   <script>  $(window).scroll(function() {
 if ($(document).scrollTop() > 150) {
     alert('hi');
