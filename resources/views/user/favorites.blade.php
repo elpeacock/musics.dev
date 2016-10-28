a2ad32fb9a8769cb380150abbbcba9ccaae04cb6
Liz Peacock <liz.peacock11@gmail.com>
2016-10-28 14:59:48 -0500

    working on building out the band prefs page

--- resources/views/user/favorites.blade.php
+++ resources/views/user/favorites.blade.php
@@ -9,81 +9,29 @@ Faves and more...
 @section('content')
 
 <div class="container">
-
     
     <div class="col-sm-4">
 
-        <form method="POST" action="{{ action('UserController@storeUserFavorites') }}">
+        <form class="form" method="POST" action="{{ action('UserController@storeUserFavorites') }}">
 
         {{ csrf_field() }}
 
-            <select name="band_id" multiple>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>6</option>
-            </select>
-            <button>submit</button>
+            <div id="bp_dropdown" class="multiselect">
+
+            @foreach($soulBands as $band_id => $value)
+
+                <a class="dropdown-item" href="#">
+
+                <input type="checkbox" id="{{ $band_id }}" name="band_id" value="{{ $band_id }}">{{ $value->name }}<i></i></a>
+
+            @endforeach
+            
+            </div>
+
         </form>
 
-        {{-- <div class="dropdown">
-
-            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-
-                R&B/Urban Soul
-
-                <span class="caret"></span>
-
-            </button>
-
-            <select class="dropdown-menu" aria-labelledby="dropdownMenu1">
-
-                @foreach($soulBands as $soulBand)
-
-                <option>{{ $soulBand->name }}</option>
-
-<<<<<<< HEAD
-                @endforeach
-=======
-    </form>
-    
-        <div class="col-sm-4">
->>>>>>> d0d5a09513521d12845c0606ebfa82646182edb1
-
-            </select>
-
-        </div> --}}
-        
-        
-    </form>
     </div>
 
 </div>
-    {{-- <form method="POST" action="{{ action('UserController@update', $user->id) }}" enctype="multipart/form-data">
 
-        {{ csrf_field() }}
-        {{ method_field('PUT') }}
-
-
-        <div class="col-sm-8 col-sm-offset-2">
-
-        <div class="form-group">
-
-            <label for="image_url" class="form-control">Add a picture</label>
-
-            <input
-                
-                type="text"
-                enctype="multipart/form-data"
-                class="form-control"
-                name="image_url"
-                placeholder=".jpg, .jpeg, .png"
-                value="{{ $user->image_url }}">
-
-        </div>
-
-        </div>
-
-    </form> --}}
+@stop
