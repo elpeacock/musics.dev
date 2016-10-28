874381c8eb8b64666d07027330763ccf9726677e
Joe Phearse <jphearse@gmail.com>
2016-10-28 10:30:09 -0500

    closed container div

--- resources/views/bands/show.blade.php
+++ resources/views/bands/show.blade.php
@@ -19,9 +19,8 @@
     </div>
     </div>
 </div> <!-- /container -->  
-
+</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-</script>
 
     <script type="text/javascript">
     $(function() {
