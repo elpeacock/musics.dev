60e1704ba76d8a644ba834d179263700bc0ab079
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-10-27 20:43:56 -0500

    Had th venue dropdown working but. commiting so joe can use events.

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -10,29 +10,31 @@
 			{{ csrf_field() }}
 			<div class="form-group">
 				<select>
-				@foreach ($bands as $band_id => $value)
-					<option value="{{ $band_id }}">{{ $value->name }}</option>
-				@endforeach
+									@foreach ($bands as $band_id => $value)
+								<option value="{{ $band_id }}">{{ $value->name }}</option>
+									@endforeach
 			</select>
-				<input type="submit" value="Submit">
-
-				//include('forms.error', ['field' => 'band'])
+							<input type="submit" value="Submit">
 			</div>
-			<div class="form-group">
-
-				//include('forms.error', ['field' => 'venue'])
-			</div>
+			{{-- <div class="form-group">
+									@foreach ($venue as $venue_id => $value)
+								<option value="{{ $venue_id }}">{{ $value->name }}</option>
+									@endforeach
+			</select>
+					<input type="submit" value="Submit">
+			</div> --}}
 			<div class="form-group">
 				<input type="date" class="form-control" name="time" id="time" placeholder="Time">
-				//include('forms.error', ['field' => 'time'])
+
 			</div>
+
 			<div class="form-group">
 				<input type="text" class="form-control" name="price" id="price" placeholder="price">
-				//include('forms.error', ['field' => 'price'])
+
 			</div>
 			<div class="form-group">
 				<input type="text" class="form-control" name="tickets" id="tickets" placeholder="Ticket Purchase Location">
-				//include('forms.error', ['field' => 'tickets'])
+
 			</div>
 
 			<button type="submit" class ="btn btn-primary pull-right">Create</button>
