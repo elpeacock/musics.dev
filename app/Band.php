a132bf01f76fe6b42265297e7ed82af50fb1544b
Liz Peacock <liz.peacock11@gmail.com>
2016-10-27 20:25:42 -0500

    added $table bands

--- app/Band.php
+++ app/Band.php
@@ -3,12 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers;
 
 class Band extends Model
 {
+	protected $table = 'bands';
 
 	public function events()
     {
         return $this->hasMany('App\Event', 'band_id');
     }
+
 }
