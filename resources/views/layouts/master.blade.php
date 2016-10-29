fbc7c1f386aaac63819fb83e455b9de4f6f15ee5
Liz Peacock <liz.peacock11@gmail.com>
2016-10-29 13:37:46 -0500

    added script tag for a main js file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -27,6 +27,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/bootstrap-select.min.js"></script>
     <!-- (Optional)JavaScript translation files for bootstrap select plugin-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/i18n/defaults-*.min.js"></script>
+    {{-- custom JS --}}
+    <script src="js/mainJs.js" type="text/javascript"></script>
 
 {{--   <script>  $(window).scroll(function() {
 if ($(document).scrollTop() > 150) {
