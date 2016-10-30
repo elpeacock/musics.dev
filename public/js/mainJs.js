8780bbe95b353a20ba987aa129c83e2a2e5b8655
Liz Peacock <liz.peacock11@gmail.com>
2016-10-30 14:14:27 -0500

    i'm not sure why but some changes i had made reverted back to a former state. eek got them back tho

--- public/js/mainJs.js
+++ public/js/mainJs.js
@@ -2,6 +2,24 @@
 
 $(document).ready(function() {
 
-    console.log( 'The DOM has finished loading!/your js is linked up yay' );
+    // js for favorites selection
+    $('#getPreferences').click(function(e) {
+        e.preventDefault();
+
+        var soul = $('.soul').selectpicker('val');
+        var rockpop = $('.rockpop').selectpicker('val');
+        var altrock = $('.altrock').selectpicker('val');
+        var rap = $('.rap').selectpicker('val');
+        var country = $('.country').selectpicker('val');
+        var jazz = $('.jazz').selectpicker('val');
+        var metal = $('.metal').selectpicker('val');
+        var dance = $('.dance').selectpicker('val');
+        var indie = $('.indie').selectpicker('val'); 
+
+        var userPrefs = soul.concat(rockpop, altrock, rap, country, jazz, metal, dance, indie);
+
+        console.log(userPrefs)
+
+    });
 
 });
