f29c7ff1bda264cc4ce52e776d5ad8b016a929f4
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-10-31 09:19:55 -0500

    fixing issues with event edit page

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -5,23 +5,41 @@
 @section('content')
 <div class="row">
 	<div class="col-xs-6 col-xs-offset-3">
-		<h4 class="modal-title">Create Event</h4>
-		<form method="POST" action="#">
+		<h2 class="modal-title">Create Event</h2>
+		<form method="POST" action="{{ action('EventsController@store') }}" enctype="multipart/form-data">
 			{{ csrf_field() }}
 			<div class="form-group">
-				<input type="text" class="form-control" name="band" id="band" {{ $event->band }}>
+				<h4 class="modal-title">Add band/bands</h4>
+				<select>
+									@foreach ($bands as $band_id => $value)
+								<option value="{{ $band_id }}">{{ $value->name }}</option>
+									@endforeach
+			</select>
+							<input type="submit" value="Submit">
 			</div>
 			<div class="form-group">
-				<input type="text" class="form-control" name="venue" id="venue" placeholder="Venue">
+					<h4 class="modal-title">Edit venue</h4>
+			<select>
+									@foreach ($venue as $venue_id => $value)
+								<option value="{{ $venue_id }}">{{ $value->name }}</option>
+									@endforeach
+			</select>
+					<input type="submit" value="Submit">
 			</div>
+
 			<div class="form-group">
+				<h4 class="modal-title">Edit event time</h4>
 				<input type="text" class="form-control" name="time" id="time" placeholder="Time">
 			</div>
+
 			<div class="form-group">
+				<h4 class="modal-title">Edit pice</h4>
 				<input type="text" class="form-control" name="price" id="price" placeholder="price">
 			</div>
+
 			<div class="form-group">
-				<input type="text" class="form-control" name="tickets" id="tickets" placeholder="Ticket Purchase Location">
+				<h4 class="modal-title">Edit Ticket URL</h4>
+				<input type="text" class="form-control" name="" id="tickets" placeholder="ticket URL">
 			</div>
 
 			<button type="submit" class ="btn btn-primary pull-right">Edit</button>
