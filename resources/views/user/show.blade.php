69b46093ab2e89e330d9182b3c4f48aecba96f39
Liz Peacock <liz.peacock11@gmail.com>
2016-10-31 17:04:37 -0500

    added users fav bands to account show page

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -2,33 +2,48 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-		{{-- <div class="col-xs-3">
-			<img class="img img-thumbnail" src="{{ $user-> }}" height="150" width="150">
-		</div> --}}
+    <div class="row">
+        {{-- <div class="col-xs-3">
+            <img class="img img-thumbnail" src="{{ $user-> }}" height="150" width="150">
+        </div> --}}
 
-	<h1>{{$user->name}}</h1>
-	<h4>User Email: {{$user->email}}</h4>
-	<h4>BaseZone: {{$user->zip_code}}</h4>
-	Memeber since {{ $user->created_at->format('F Y') }}
-			@if(Auth::check() && (Auth::user()->id == $user->id))
-			<div>
-				<a class="btn btn-default" href="{{ action('UserController@edit', $user->id) }}">Change your info</a>
-			</div>
-			@endif
+    <h1>{{$user->name}}</h1>
+    <h4>User Email: {{$user->email}}</h4>
+    <h4>BaseZone: {{$user->zip_code}}</h4>
+    Memeber since {{ $user->created_at->format('F Y') }}
+            @if(Auth::check() && (Auth::user()->id == $user->id))
+            <div>
+                <a class="btn btn-default" href="{{ action('UserController@edit', $user->id) }}">Change your info</a>
+            </div>
+            @endif
 
+    </div>
+    {{-- @if(Auth::id() == $users->id)
+        <a href="{{action('UserController@edit', $users->id)}}"><button type="button" class="btn btn-warning">Edit <i class="fa fa-pencil"></i></button></a>
+    @endif --}}
+    <hr>
 
-	{{-- @if(Auth::id() == $users->id)
-		<a href="{{action('UserController@edit', $users->id)}}"><button type="button" class="btn btn-warning">Edit <i class="fa fa-pencil"></i></button></a>
-	@endif --}}
-	<hr>
+    <div>
+    <h1>Favorite Bands</h1>
+    <ul>
+    @foreach($user->bandPreferences as $favorite)
+        <li class="col-sm-3">{{ $favorite->name }}</li>
+    @endforeach
+    </ul>
+    </div>
 
-	<h1>RSVPs</h1>
-	@foreach($user->events as $event)
-		<a href="{{action('EventsController@show', $event->id)}}"><h3>{{$event->band->name}}</h3></a>
-		<h4>Location: {{$event->venue->name}}</h4>
-		<h4>Date: {{$event->event_time}}</h4>
-		<h4>Ticket Price: ${{$event->price}}</h4>
-		<hr>
-	@endforeach
+    <hr>
+
+    <div>
+    <h1>RSVPs</h1>
+    @foreach($user->events as $event)
+        <a href="{{action('EventsController@show', $event->id)}}"><h3>{{$event->band->name}}</h3></a>
+        <h4>Location: {{$event->venue->name}}</h4>
+        <h4>Date: {{$event->event_time}}</h4>
+        <h4>Ticket Price: ${{$event->price}}</h4>
+        <hr>
+    @endforeach
+    </div>
+
+</div>
 @stop
