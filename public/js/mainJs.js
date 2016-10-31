b8ca343d9aabe27e05e5b08616919e0971502835
Liz Peacock <liz.peacock11@gmail.com>
2016-10-31 13:27:01 -0500

    wrote a bunch of js that i didn't need - took it out.

--- public/js/mainJs.js
+++ public/js/mainJs.js
@@ -2,37 +2,5 @@
 
 $(document).ready(function() {
 
-    // js for favorites selection
-    $('#getPreferences').click(function(e) {
-        e.preventDefault();
-
-        var soul = $('.soul').selectpicker('val');
-        var rockpop = $('.rockpop').selectpicker('val');
-        var altrock = $('.altrock').selectpicker('val');
-        var rap = $('.rap').selectpicker('val');
-        var country = $('.country').selectpicker('val');
-        var jazz = $('.jazz').selectpicker('val');
-        var metal = $('.metal').selectpicker('val');
-        var dance = $('.dance').selectpicker('val');
-        var indie = $('.indie').selectpicker('val'); 
-
-        var userPrefs = soul.concat(rockpop, altrock, rap, country, jazz, metal, dance, indie);
-
-        sendUserPrefs(userPrefs);
-
-    });
-
-    function sendUserPrefs() {
-        $.ajax({
-            url: 'UserController@storeUserFavorites', 
-            type: 'POST', 
-            data: {data : userPrefs}, 
-            dataType: 'text', 
-            success: function() {
-                console.log('favorites sent');
-            };
-        });
-
-    };
-
+    
 });
