3f990e547c54e3025ad6d604bedc58b0cfc6965b
Joe Phearse <jphearse@gmail.com>
2016-11-01 15:08:08 -0500

    tried to do file stack

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+{{-- <script type="text/javascript"
+ src="//api.filestackapi.com/filestack.js"></script>
+
+<script type="text/javascript">
+    
+    function proImg (img){
+                var imageSpace = "" 
+                imageSpace += "<img src=" + event.fpfile.url + ">"
+                $("#image").html(imageSpace);
+
+        }
+</script>
+ --}}
 <div class="container">
+{{-- <div class="col-lg-12">
+        <div id="image"></div>
+
+        <input type="filepicker" data-fp-apikey="AP4T0YV1TJGQi2wRdpdmwz" data-fp-mimetypes="image/*" data-fp-container="modal" data-fp-services="COMPUTER,FACEBOOK,GITHUB" onchange="proImg()">
+</div> --}}
 <div class="row">
 @if(session()->has('SUCCESS_MESSAGE'))
         <div class="alert alert-success">
@@ -11,7 +31,6 @@
         {{-- <div class="col-xs-3">
             <img class="img img-thumbnail" src="{{ $user-> }}" height="150" width="150">
         </div> --}}
-
     <h1>{{$user->name}}</h1>
     <h4>User Email: {{$user->email}}</h4>
     <h4>BaseZone: {{$user->zip_code}}</h4>
