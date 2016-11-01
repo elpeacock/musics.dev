510395e348fc4e362d60d860251ed8b93b9f161f
Liz Peacock <liz.peacock11@gmail.com>
2016-11-01 16:11:40 -0500

    working on seeds for events

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -11,21 +11,85 @@ class EventsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('events')->insert([
-            'band_id' => 1,
-            'venue_id' => 1, 
-            'event_time' => '8:00 pm',
-            'price' => 10.00,
-            'buy_tickets' => 'ticketfly.com, or at the venue',
-        ]);
+        // DB::table('events')->insert([
+        //     'band_id' => 393,
+        //     'venue_id' => 1, 
+        //     'event_time' => '2016-11-10 20:00:00',
+        //     'price' => 25.00,
+        //     'buy_tickets' => 'livenation.com, additional fees applied at checkout',
+        // ]);
 
-        DB::table('events')->insert([
-            'band_id' => 2,
-            'venue_id' => 2, 
-            'event_time' => '8:00 pm',
-            'price' => 10.00,
-            'buy_tickets' => 'ticketfly.com, or at the venue',
-        ]);
+        // DB::table('events')->insert([
+        //     'band_id' => 570,
+        //     'venue_id' => 2, 
+        //     'event_time' => '2016-12-01 21:00:00',
+        //     'price' => 129.50,
+        //     'buy_tickets' => 'ticketmaster.com, additional fees applied at checkout',
+        // ]);
+
+        // DB::table('events')->insert([
+        //     'band_id' => 904,
+        //     'venue_id' => 2, 
+        //     'event_time' => '2016-11-09 20:00:00',
+        //     'price' => 20.00,
+        //     'buy_tickets' => 'ticketweb.com, additional fees applied at checkout',
+        // ]);
+
+        // DB::table('events')->insert([
+        //     'band_id' => 578,
+        //     'venue_id' => 2, 
+        //     'event_time' => '2016-11-19 21:00:00',
+        //     'price' => 20.00,
+        //     'buy_tickets' => 'ticketweb.com, additional fees applied at checkout',
+        // ]);
+
+        // DB::table('events')->insert([
+        //     'band_id' => 1128,
+        //     'venue_id' => 2, 
+        //     'event_time' => '2016-11-22 20:00:00',
+        //     'price' => 70.00,
+        //     'buy_tickets' => 'ticketmaster.com, additional fees applied at checkout',
+        // ]);
+
+        // DB::table('events')->insert([
+        //     'band_id' => 607,
+        //     'venue_id' => 2, 
+        //     'event_time' => '2016-11-29 20:00:00',
+        //     'price' => 77.00,
+        //     'buy_tickets' => 'livenation.com, additional fees applied at checkout',
+        // ]);
+
+        // DB::table('events')->insert([
+        //     'band_id' => 859,
+        //     'venue_id' => 2, 
+        //     'event_time' => '2016-12-03 20:00:00',
+        //     'price' => 17.00,
+        //     'buy_tickets' => 'ticketweb.com, additional fees applied at checkout',
+        // ]);
+
+        // DB::table('events')->insert([
+        //     'band_id' => 1078,
+        //     'venue_id' => 2, 
+        //     'event_time' => '2016-12-04 19:30:00',
+        //     'price' => 125.00,
+        //     'buy_tickets' => 'ticketmaster.com, additional fees applied at checkout',
+        // ]);
+
+        // DB::table('events')->insert([
+        //     'band_id' => 115,
+        //     'venue_id' => 2, 
+        //     'event_time' => '2016-12-31 21:00:00',
+        //     'price' => 65.00,
+        //     'buy_tickets' => 'livenation.com, additional fees applied at checkout',
+        // ]);
+
+        // DB::table('events')->insert([
+        //     'band_id' => 860,
+        //     'venue_id' => 2, 
+        //     'event_time' => '2017-01-05 19:00:00',
+        //     'price' => 101.00,
+        //     'buy_tickets' => 'ticketmaster.com, additional fees applied at checkout',
+        // ]);
 
         
     }
