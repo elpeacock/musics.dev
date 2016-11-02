cd873fef20b5fb7d76df1a0d4e19cf33e1d668eb
Liz Peacock <liz.peacock11@gmail.com>
2016-11-02 10:25:10 -0500

    fixed the offset between the headline and all bands list

--- resources/views/bands/index.blade.php
+++ resources/views/bands/index.blade.php
@@ -6,28 +6,30 @@
 
 <div class="container">
 
-<h1>All Bands</h1>
+        <h1>All Bands</h1>
 
-<div class="container">
+<div class="row">
 
 @foreach($bands as $band)
+        
     <div class="col-sm-4 col-lg-4 col-md-4">
 
         <div class="box thumbnail animated">
 
           {{-- <img class="imgSize" src="http://placekitten.com/200/200" alt=""> --}}
 
-          <div class="caption">
+            <div class="caption">
 
-            <h4><a href="{{action('BandsController@show', $band->id)}}">{{ $band->name }}</a></h4>
+                <h4><a href="{{action('BandsController@show', $band->id)}}">{{ $band->name }}</a></h4>
 
-          </div>
+            </div>
 
         </div> <!-- /.box thumbnail -->
 
     </div> <!-- /.col-sm-4 col-lg-4 col-md-4 -->
+        
 @endforeach
-</div>
+</div> <!-- /.row -->
 
     <div class="text-center">
 
