fea940937b4eddf742fe6217b6f3103e2c369c7b
Liz Peacock <liz.peacock11@gmail.com>
2016-11-01 20:20:43 -0500

    cleaned up code, made sure all tags were closed, tabs on point and converted to spacing wrapped pagination in text-center div

--- resources/views/bands/index.blade.php
+++ resources/views/bands/index.blade.php
@@ -5,22 +5,35 @@
 @section('content')
 
 <div class="container">
+
 <h2>All Bands</h2>
+
 <div class="container">
+
 @foreach($bands as $band)
-      <div class="col-sm-4 col-lg-4 col-md-4">
+    <div class="col-sm-4 col-lg-4 col-md-4">
+
         <div class="box thumbnail animated">
 
           {{-- <img class="imgSize" src="http://placekitten.com/200/200" alt=""> --}}
 
           <div class="caption">
+
             <h4><a href="{{action('BandsController@show', $band->id)}}">{{ $band->name }}</a></h4>
-            <p><p>
+
           </div>
 
         </div> <!-- /.box thumbnail -->
-      </div> <!-- /.col-sm-4 col-lg-4 col-md-4 -->
+
+    </div> <!-- /.col-sm-4 col-lg-4 col-md-4 -->
 @endforeach
 </div>
-{!! $bands->render() !!}
+
+    <div class="text-center">
+
+        {!! $bands->render() !!}
+        
+    </div>
+
+</div>
 @stop
