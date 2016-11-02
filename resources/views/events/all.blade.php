a285465242e539af5dd41601008474e71cef4633
Liz Peacock <liz.peacock11@gmail.com>
2016-11-01 20:23:02 -0500

    cleaned up code, made sure tabs were consistent/done with spacing

--- resources/views/events/all.blade.php
+++ resources/views/events/all.blade.php
@@ -6,23 +6,31 @@
 @section('content')
 
 <div class="container">
-<h2>All Events</h2>
-@foreach($events as $event)
-	      <div class="media">
+
+    <h1>All Events</h1>
+
+    @foreach($events as $event)
+
+    <div class="media">
+
         <a class="pull-left" href="#">
-        <img class="media-object" src="http://placekitten.com/250/180">
-      </a>
-      <div class="media-body">
-        <a href="{{action('EventsController@show', $event->id)}}"><h3>{{$event->band->name}}</h3></a>
-        <h4>Location: {{$event->venue->name}}</h4>
-        <h4>Date: {{$event->event_time}}</h4>
-        <h4>Ticket Price: ${{$event->price}}</h4>
-				<h4> Ticket Location: {{$event->buy_tickets}}</h4>
-        <hr>
-       </div>
+            <img class="media-object" src="http://placekitten.com/250/180">
+        </a>
+
+        <div class="media-body">
+
+            <a href="{{action('EventsController@show', $event->id)}}"><h3>{{$event->band->name}}</h3></a>
+            <h4>Location: {{$event->venue->name}}</h4>
+            <h4>Date: {{$event->event_time}}</h4>
+            <h4>Ticket Price: ${{$event->price}}</h4>
+            <h4> Ticket Location: {{$event->buy_tickets}}</h4>
+
+        </div>
+        
     </div>
+
     <hr>
-@endforeach
+    @endforeach
 </div>
 
 @stop
