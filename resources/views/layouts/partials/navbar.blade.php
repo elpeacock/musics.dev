5f60eab6dd2e4de5c2e1bb3eef02399192bb4e51
Liz Peacock <liz.peacock11@gmail.com>
2016-11-01 20:28:30 -0500

    made styling edits to navbar

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -57,32 +57,38 @@
         
         <ul class="nav navbar-nav pull-right">
             
-     @if(Auth::check())
-            <li class="dropdown">
-                
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}<b class="caret"></b></a>
-                
-                <ul class="dropdown-menu">
-                    
-                    <li><a href="{{ action('UserController@show', Auth::id()) }}">Account Info</a></li>
+    @if(Auth::check())
 
-                    <li><a href="{{ action('UserController@pickFavoriteBands') }}">Pick Your Faves</a></li>
-                    
-                    <li><a href="{{ action('EventsController@create') }}">Create An Event</a></li>
-                    
-                    <li class="divider"></li>
-                    <li><a href="{{ action('Auth\AuthController@getLogout') }}">Log Out</a></li>  
-                </ul>
-      @else      
-            <li><a href="{{ action('Auth\AuthController@getRegister') }}">Sign Up</a></li>
+        <li class="dropdown">
             
-            <li><a href="{{ action('Auth\AuthController@getLogin') }}">Log In</a></li>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}<b class="caret"></b></a>
+            
+            <ul class="dropdown-menu">
+                
+                <li><a href="{{ action('UserController@show', Auth::id()) }}">Account Info</a></li>
+
+                <li><a href="{{ action('UserController@pickFavoriteBands') }}">Pick Your Faves</a></li>
+                
+                <li><a href="{{ action('EventsController@create') }}">Create An Event</a></li>
+                
+                <li class="divider"></li>
+                <li><a href="{{ action('Auth\AuthController@getLogout') }}">Log Out</a></li>  
+            </ul>
+    @else
+
+        <li><a href="{{ action('Auth\AuthController@getRegister') }}">Sign Up</a></li>
+        
+        <li><a href="{{ action('Auth\AuthController@getLogin') }}">Log In</a></li>
+
     @endif               
 
-            <li><a href="{{ action('EventsController@index') }}">Events</a></li>
-            <li><a href="{{ action('BandsController@index') }}">Bands</a></li>
-            
-            </li>
+        <li><a href="{{ action('EventsController@index') }}">Events</a></li>
+
+        <li><a href="{{ action('BandsController@index') }}">Bands</a></li>
+
+        <li><button class="btn btn-default navbar-search" id="toggler"><i class="glyphicon glyphicon-search"></i></button></li>
+        
+        </li>
        
         </ul>
 
