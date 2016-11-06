76f672b9ddbd4c2ffa75549351bf5d01ac38a490
Liz Peacock <liz.peacock11@gmail.com>
2016-11-06 15:31:20 -0600

    cleaning code, added comments re:relationships

--- app/Band.php
+++ app/Band.php
@@ -9,21 +9,25 @@ class Band extends Model
 {
 	protected $table = 'bands';
 
+    // band to event relationship
 	public function events()
     {
         return $this->hasMany('App\Event', 'band_id');
     }
 
+    // band to user faves relationship
     public function usersFavorites()
     {
         return $this->belongsToMany('App\User', 'user_bands', 'band_id', 'user_id');
     }
 
+    // band to genre relationship
     public function genres()
     {
         return $this->belongsTo('App\Genre', 'genre_id');
     }
 
+    // band search function
     public static function searchBand($searchTerm)
     {
         return self::where('name', 'LIKE', '%' . $searchTerm . '%');
